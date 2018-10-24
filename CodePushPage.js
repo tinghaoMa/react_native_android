ab8ec73fd31d5133b6b076075dc8cca6e985306a
tinghaoma <tinghaoma@sohu-inc.com>
2018-10-24 20:44:48 +0800

    完成对android热更新

--- CodePushPage.js
+++ CodePushPage.js
@@ -29,13 +29,23 @@ export default class CodePushPage extends React.Component {
     }
 
     checkUpdate() {
-        codePush.sync()
+        codePush.sync({
+            updateDialog: {
+                appendReleaseDescription: true,
+                descriptionPrefix:'\n\n更新内容：\n',
+                title:'更新',
+                mandatoryUpdateMessage:'',
+                mandatoryContinueButtonLabel:'更新',
+            },
+            mandatoryInstallMode:codePush.InstallMode.IMMEDIATE,
+        });
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1
+
     },
     text: {
         color: 'red',
